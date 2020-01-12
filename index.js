38da67ec1feec02ea7c7facf37e36b2f27ceced7
Asazuke11 <quusan3@gmail.com>
2020-01-12 19:42:37 +0900

    foodのデータベース

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const http = require('http');
 const auth = require('http-auth');
 const router = require('./lib/router');
 
-const basic = auth.basic({
-  realm: 'Enter username and password.',
-  file: './users.htpasswd'
-});
+// const basic = auth.basic({
+//   realm: 'Enter username and password.',
+//   file: './users.htpasswd'
+// });
 
-const server = http.createServer(basic, (req, res) => {
+const server = http.createServer((req, res) => {
   router.route(req, res);
 }).on('error', (e) => {
   console.error('Server Error', e);
